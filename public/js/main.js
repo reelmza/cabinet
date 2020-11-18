26529845c5b3bae60dd8519c2999524cbfa4b700
Jesse Moses <jessemoses71@gmail.com>
2020-11-19 00:15:04 +0100

    Made teacher profile editable

--- public/js/main.js
+++ public/js/main.js
@@ -1,138 +1,235 @@
 // Welcome Box elements
-const welcomeBox = document.querySelector('#welcome-box')
+const welcomeBox = document.querySelector("#welcome-box");
 
 // Login Form elements
-const loginButton = document.querySelector('#login-button')
-const loginFormBox = document.querySelector('#login-form-box')
-const loginForm = document.querySelector('#login-form')
-const loginToRegister = document.querySelector('#login-register-link')
-const loginError = document.querySelector('.myAlert')
+const loginButton = document.querySelector("#login-button");
+const loginFormBox = document.querySelector("#login-form-box");
+const loginForm = document.querySelector("#login-form");
+const loginToRegister = document.querySelector("#login-register-link");
+const loginError = document.querySelector(".myAlert");
 
-    // LoginForm Data
-    const loginUsername = document.querySelector('#login-username')
-    const loginPassword = document.querySelector('#login-password')
-    const loginPerson = document.querySelector("#login-type")
+// LoginForm Data
+const loginUsername = document.querySelector("#login-username");
+const loginPassword = document.querySelector("#login-password");
+const loginPerson = document.querySelector("#login-type");
 
 // Register Form elements
-const registerButton = document.querySelector('#register-button')
-const registerFormBox = document.querySelector('#register-form-box')
-const registerForm = document.querySelector('#register-form')
-const registerToLogin = document.querySelector('#register-login')
+const registerButton = document.querySelector("#register-button");
+const registerFormBox = document.querySelector("#register-form-box");
+const registerForm = document.querySelector("#register-form");
+const registerToLogin = document.querySelector("#register-login");
 
 // Main
-const main = document.querySelector('#main')
-const appForms = document.querySelector('#app-forms')
+const main = document.querySelector("#main");
+const appForms = document.querySelector("#app-forms");
 
 // Header Container
-const headerContainer = document.querySelector('#header-container')
-const sideBar = document.querySelector('#side-bar')
-const mainBar = document.querySelector('#main-bar')
+const headerContainer = document.querySelector("#header-container");
+const sideBar = document.querySelector("#side-bar");
+const mainBar = document.querySelector("#main-bar");
 
 // Register /Login dropdown
-const dropDownCaller = document.querySelector('#sign-in-icon')
-const userDropdown = document.querySelector('#user-dropdown')
-const closeDropdown = document.querySelector('#close-dropdown')
+const dropDownCaller = document.querySelector("#sign-in-icon");
+const closeDropdown = document.querySelector("#close-dropdown");
 
 // Main Drop Down
-const mainDropdown = document.querySelector('#main-dropdown')
-const dropDownIcon = document.querySelector('#dropdown-icon')
-const closeMainDropdown = document.querySelector('#close-main-dropdown')
+const mainDropdown = document.querySelector("#main-dropdown");
+const dropDownIcon = document.querySelector("#dropdown-icon");
+const closeMainDropdown = document.querySelector("#close-main-dropdown");
 
+// Global Funtions
+const loadItems = (data) => {
+  // Teacher Form Name / Other elements where teacher name appears
+  // Name
+  document.querySelectorAll(".teacher-name").forEach((element) => {
+    element.textContent = data.name;
+  });
+
+  document
+    .querySelector(".teacher-f-name")
+    .setAttribute("placeholder", data.name);
+
+  // feild
+  document.querySelectorAll(".teacher-feild").forEach((element) => {
+    element.textContent = data.feild;
+  });
+
+  document
+    .querySelector(".teacher-f-feild")
+    .setAttribute("placeholder", data.feild);
+
+  // Age
+  document
+    .querySelector(".teacher-f-age")
+    .setAttribute("placeholder", data.age);
+
+  // phone
+  document
+    .querySelector(".teacher-f-phone")
+    .setAttribute("placeholder", data.phone);
+
+  // Mail
+  document
+    .querySelector(".teacher-f-mail")
+    .setAttribute("placeholder", data.mail);
+
+  // Address
+  document
+    .querySelector(".teacher-f-address")
+    .setAttribute("placeholder", data.address);
+
+  // username
+  document
+    .querySelector(".teacher-f-username")
+    .setAttribute("value", data.username);
+};
 
 // Login functions
-    // Display login form & Hide welcome box
-    loginButton.addEventListener('click', (e) => {
-        e.preventDefault()
+// Display login form & Hide welcome box
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-        //Hide dull main container
-        main.setAttribute('style', 'display:none;')
-        appForms.setAttribute('style', 'display:flex;')
-        headerContainer.setAttribute('style', 'display:none;')
-        
-        //Show login form
-        loginFormBox.setAttribute('style', 'display:flex')
-    })
+  //Hide dull main container
+  main.setAttribute("style", "display:none;");
+  appForms.setAttribute("style", "display:flex;");
+  headerContainer.setAttribute("style", "display:none;");
 
-    registerToLogin.addEventListener('click', (e) => {
-        e.preventDefault()
+  //Show login form
+  loginFormBox.setAttribute("style", "display:flex");
+});
 
-        //Hide dull main container
-        main.setAttribute('style', 'display:none;')
-        appForms.setAttribute('style', 'display:flex;')
-        headerContainer.setAttribute('style', 'display:none;')
-        registerFormBox.setAttribute('style', 'display:none;')
-        
-        //Show login form
-        loginFormBox.setAttribute('style', 'display:flex')
-    })
-    // Login function
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        
-        // Data from the form
-        const username = loginUsername.value.toLowerCase().split(" ").join("")
-        const password = loginPassword.value.toLowerCase().split(" ").join("")
+registerToLogin.addEventListener("click", (e) => {
+  e.preventDefault();
 
-        fetch("/login?username=" + username + "&password=" + password).then((response) => {
-            response.json().then((data) => {
-                if(data.error) {
-                    console.log(data.error)
-                    loginError.setAttribute('style', 'display:block; color:red; margin-bottom:5px; animation:fade 1s;')
+  //Hide dull main container
+  main.setAttribute("style", "display:none;");
+  appForms.setAttribute("style", "display:flex;");
+  headerContainer.setAttribute("style", "display:none;");
+  registerFormBox.setAttribute("style", "display:none;");
 
-                    return setTimeout(()=>{
-                        loginError.setAttribute('style', 'display:none;')
-                    }, 3000)
+  //Show login form
+  loginFormBox.setAttribute("style", "display:flex");
+});
+// Login function
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-                }  
+  // Data from the form
+  const username = loginUsername.value.toLowerCase().split(" ").join("");
+  const password = loginPassword.value.toLowerCase().split(" ").join("");
 
-                console.log(data.message)
-                appForms.setAttribute('style', 'display:none !important')
-                mainBar.setAttribute('class', 'col-12 col-md-8 mt-2')
+  fetch("/login?username=" + username + "&password=" + password).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          loginError.setAttribute(
+            "style",
+            "display:block; color:red; margin-bottom:5px; animation:fade 1s;"
+          );
 
-                main.setAttribute('style', 'display:flex;')
-                headerContainer.setAttribute('style', 'display:flex;')
-                userDropdown.setAttribute('style', 'display:none;')
-                sideBar.setAttribute('style', 'display:flex;')
+          return setTimeout(() => {
+            loginError.setAttribute("style", "display:none;");
+          }, 3000);
+        } else {
+          console.log(data.message);
+          appForms.setAttribute("style", "display:none !important");
+          mainBar.setAttribute("class", "col-12 col-md-8 mt-2");
 
-                const name = document.querySelector('#name')
-                const nameUnder = document.querySelector('#title')
+          main.setAttribute("style", "display:flex;");
+          headerContainer.setAttribute("style", "display:flex;");
+          sideBar.setAttribute("style", "display:flex;");
 
-                name.textContent = data.name
-                nameUnder.textContent = data.username
-            })
-        })
-    })
-
+          loadItems(data);
+        }
+      });
+    }
+  );
+});
 
 // Register functions
-    // Display register form & Hide welcome box
-    registerButton.addEventListener('click', (e) => {
-        e.preventDefault()
+// Display register form & Hide welcome box
+registerButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-        //Hide dull welcome-box & Login box already on display
-        main.setAttribute('style', 'display:none;')
-        appForms.setAttribute('style', 'display:flex;')
-        headerContainer.setAttribute('style', 'display:none;')
-        loginFormBox.setAttribute('style', 'display:none;')
-        
-        //Show register form
-        registerFormBox.setAttribute('style', 'display:flex')
-    })
+  //Hide dull welcome-box & Login box already on display
+  main.setAttribute("style", "display:none;");
+  appForms.setAttribute("style", "display:flex;");
+  headerContainer.setAttribute("style", "display:none;");
+  loginFormBox.setAttribute("style", "display:none;");
 
-    loginToRegister.addEventListener('click', (e) => {
-        e.preventDefault()
+  //Show register form
+  registerFormBox.setAttribute("style", "display:flex");
+});
 
-        //Hide dull welcome-box & Login box already on display
-        main.setAttribute('style', 'display:none;')
-        appForms.setAttribute('style', 'display:flex;')
-        headerContainer.setAttribute('style', 'display:none;')
-        loginFormBox.setAttribute('style', 'display:none;')
-        
-        //Show register form
-        registerFormBox.setAttribute('style', 'display:flex')
-    })
+loginToRegister.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    // Register Function
-    registerForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-    })
+  //Hide dull welcome-box & Login box already on display
+  main.setAttribute("style", "display:none;");
+  appForms.setAttribute("style", "display:flex;");
+  headerContainer.setAttribute("style", "display:none;");
+  loginFormBox.setAttribute("style", "display:none;");
+
+  //Show register form
+  registerFormBox.setAttribute("style", "display:flex");
+});
+
+// Register Function
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Teacher profile functions
+const teacherSave = document.querySelector("#profile-save");
+const teacherForm = document.querySelector("#profile-form");
+
+// Enable disable inputs
+document.querySelector("#profile-edit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.querySelectorAll(".disabled").forEach((element) => {
+    element.removeAttribute("disabled");
+  });
+
+  teacherSave.removeAttribute("disabled");
+  teacherSave.setAttribute("style", "color:#fff");
+});
+
+//Edit Function
+teacherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector(".teacher-f-username").value;
+  const name = document.querySelector(".teacher-f-name").value.toUpperCase();
+  const age = document.querySelector(".teacher-f-age").value;
+  const address = document.querySelector(".teacher-f-address").value;
+  const feild = document.querySelector(".teacher-f-feild").value;
+  const mail = document.querySelector(".teacher-f-mail").value;
+  const phone = document.querySelector(".teacher-f-phone").value;
+
+  fetch(
+    "/editUser?username=" +
+      username +
+      "&name=" +
+      name +
+      "&age=" +
+      age +
+      "&address=" +
+      address +
+      "&feild=" +
+      feild +
+      "&mail=" +
+      mail +
+      "&phone=" +
+      phone
+  ).then((response) => {
+    response.json().then((data) => {
+      if (data.success) {
+        loadItems(data.newInfo);
+      } else {
+        console.log("Failed");
+      }
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,97 +1,128 @@
 // Import Libraries
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const fs = require('fs')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const fs = require("fs");
 
 // Functions
-const functions = require('./utils/functions.js')
+const functions = require("./utils/functions.js");
 
 // Shortened Variables
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define Paths
-const publicPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Set hbs views engine
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
-hbs.registerPartials(partialsPath)
+hbs.registerPartials(partialsPath);
 
 // Set front end folder (Static Files)
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 // Main Content
 // Index page
-app.get('', (req, res) => {
-    res.render('index', {
-        name:'CABINET'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    name: "CABINET",
+  });
+});
 
-app.get('/login', (req,res) => {
-    // Login
-    var error;
-    const username = req.query.username
-    const password = req.query.password
+app.get("/login", (req, res) => {
+  // Login
+  var error;
+  const username = req.query.username;
+  const password = req.query.password;
 
-    const users = functions.readUsers()[0].users
-    const target = users.find((user) => user.username === username && user.password === password)
+  const users = functions.readUsers()[0].users;
+  const target = users.find(
+    (user) => user.username === username && user.password === password
+  );
 
-    if(target) {
+  if (target) {
+    console.log("user Found");
+    return res.send({
+      username: target.username,
+      message: "Welcome " + target.name,
+      name: target.name,
+      feild: target.feild,
+      age: target.age || "Not Added",
+      address: target.address || "Not Added",
+      phone: target.phone || "Not Added",
+      mail: target.mail || "Not Added",
+    });
+  } else {
+    console.log("Username or Password incorect");
+    res.send({ error: "Username or Password incorect" });
+  }
+});
 
-        console.log('user Found')
-        return res.send({
-            message: 'Welcome ' + target.name,
-            name: target.name,
-            username: target.username
-        })
-    } else {
+app.get("/editUser", (req, res) => {
+  const username = req.query.username;
+  const name = req.query.name;
+  const age = req.query.age;
+  const address = req.query.address;
+  const feild = req.query.feild;
+  const mail = req.query.mail;
+  const phone = req.query.phone;
 
-        console.log('Username or Password incorect')
-        res.send({error: 'Username or Password incorect'})
-    }
-})
+  // Edit user details
+  const db = functions.readUsers();
+  const dbUsers = functions.readUsers()[0].users;
+  const userToEdit = dbUsers.find((user) => user.username === username);
 
-app.get('/editUser', (req, res) => {
-    const username = req.query.username
-
-    // Edit user details
-    const db = functions.readUsers()
-    const dbUsers = functions.readUsers()[0].users
-    const userToEdit = dbUsers.find((user) => user.name === username)
-
-    if(!userToEdit) {
-        console.log('User Not Found!')
-    } else {
-
+  if (!userToEdit) {
+    console.log("User Not Found!");
+  } else {
     // Edit details
-    userToEdit.age = 10
-    // console.log(dbUsers)
-
-    // Push details 
-    db.pop()
-    db.push({
-        users:dbUsers
-    })
-
-    const editedData = JSON.stringify(db)
-    fs.writeFileSync(__dirname + '/utils/db/users.json', editedData, 'utf-8')
-
+    if (name) {
+      userToEdit.name = name;
     }
-})
+    if (age) {
+      userToEdit.age = age;
+    }
+    if (address) {
+      userToEdit.address = address;
+    }
+    if (feild) {
+      userToEdit.feild = feild;
+    }
+    if (mail) {
+      userToEdit.mail = mail;
+    }
+    if (phone) {
+      userToEdit.phone = phone;
+    }
+
+    console.log(userToEdit);
+
+    // Push details
+    db.pop();
+    db.push({
+      users: dbUsers,
+    });
+    const editedData = JSON.stringify(db);
+    fs.writeFileSync(__dirname + "/utils/db/users.json", editedData, "utf-8");
+
+    return res.send({
+      success: "Edited Successfully",
+      newInfo: userToEdit,
+    });
+  }
+});
 // Handle 404 for index
-app.get('/*', (req, res) => {
-    res.render('404', {
-        name:'CABINET'
-    })
-})
+app.get("/*", (req, res) => {
+  res.render("404", {
+    name: "CABINET",
+  });
+});
 
 // Run App
 app.listen(port, () => {
-    console.log('App Running...')
-})
+  console.log("App Running...");
+});
